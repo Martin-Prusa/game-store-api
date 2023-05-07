af317d98109a1fd4b25c4eacf85d795f7ebac8a6
mirekbartunek <mucus.hooide@gmail.com>
2023-05-07 22:20:01 +0200

    enabled cors for localhost:3001

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors({
+    origin: `http://localhost:3001`,
+  });
   const config = new DocumentBuilder()
     .setTitle('Game store API')
     .setVersion('1.0')
@@ -16,4 +18,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
